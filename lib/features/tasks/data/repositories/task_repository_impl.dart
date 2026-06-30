de6cbb0d7b341ebf15bdd63062f15908f91efc70
leonardo-pauli <leonardopaulisantos@gmail.com>
2026-06-30 10:30:25 -0300

    feat(data): finaliza os metodos de crud no repositorio do hive

--- lib/features/tasks/data/repositories/task_repository_impl.dart
+++ lib/features/tasks/data/repositories/task_repository_impl.dart
@@ -1,28 +1,50 @@
+import 'package:hive/hive.dart';
+import 'package:taskflow/features/tasks/data/models/task_model.dart';
 import 'package:taskflow/features/tasks/domain/entity/task_entity.dart';
 import 'package:taskflow/features/tasks/domain/repositories/itask_repository.dart';
 
 class TaskRepositoryImpl implements ITaskRepository{
- final dynamic box;
+ final Box box;
 
  TaskRepositoryImpl({required this.box});
 
   @override
-  Future<void> deleteTask(String id) {
-    throw UnimplementedError();
+  Future<void> deleteTask(String id) async {
+    await box.delete(id);
   }
 
   @override
-  Future<List<TaskEntity>> getAllTasks() {
-    throw UnimplementedError();
+  Future<List<TaskEntity>> getAllTasks() async {
+    final results = box.values;
+    return results.map((e) => 
+    TaskModel.fromMap(
+      Map<String, dynamic>.from(e)))
+      .toList();
   }
 
   @override
-  Future<void> saveTask(TaskEntity task) {
-    throw UnimplementedError();
+  Future<void> saveTask (TaskEntity task) async {
+      final boxModel = TaskModel(
+      createdAt: task.createdAt, 
+      id: task.id, 
+      priority: task.priority, 
+      status: task.status, 
+      title: task.title, 
+      description: task.description,
+      );
+      await box.put(task.id, boxModel.toMap());
   }
 
   @override
-  Future<void> updateTask(TaskEntity task) {
-    throw UnimplementedError();
+  Future<void> updateTask(TaskEntity task) async {
+    final boxModel = TaskModel(
+      createdAt: task.createdAt, 
+      id: task.id, 
+      priority: task.priority, 
+      status: task.status, 
+      title: task.title, 
+      description: task.description,
+      );
+      await box.put(task.id, boxModel.toMap());
   }
 }
