c3163007cdd488e40a673a1e203c82ee23f910c1
leonardo-pauli <leonardopaulisantos@gmail.com>
2026-07-03 08:48:54 -0300

    feat(core): setup get_it dependency injection with hive and usecases

---
+++ lib/domain/task_entity.dart
@@ -0,0 +1,39 @@
+enum TaskStatus { todo, doing, done }
+
+enum TaskPriority { urgent, important, normal }
+
+class TaskEntity {
+  final String id;
+  final String title;
+  final String? description;
+  final DateTime createdAt;
+  final TaskStatus status;
+  final TaskPriority priority;
+
+  const TaskEntity({
+    required this.createdAt,
+    required this.id,
+    required this.priority,
+    required this.status,
+    required this.title,
+    this.description,
+  });
+
+  TaskEntity copyWith({
+    String? id,
+    String? title,
+    String? description,
+    DateTime? createdAt,
+    TaskStatus? status,
+    TaskPriority? priority,
+  }) {
+    return TaskEntity(
+      createdAt: createdAt ?? this.createdAt,
+      description: description ?? this.description,
+      id: id ?? this.id,
+      priority: priority ?? this.priority,
+      status: status ?? this.status,
+      title: title ?? this.title,
+    );
+  }
+}
--- lib/features/tasks/domain/usecases/save_task_usecase.dart
+++ lib/features/tasks/domain/usecases/save_task_usecase.dart
@@ -1,14 +1,13 @@
-import 'package:taskflow/features/tasks/domain/entity/task_entity.dart';
+import 'package:taskflow/domain/task_entity.dart';
+
 import 'package:taskflow/features/tasks/domain/repositories/itask_repository.dart';
 
 class SaveTaskUseCase {
   final ITaskRepository repository;
 
- SaveTaskUseCase({required this.repository});
+  SaveTaskUseCase({required this.repository});
 
-   Future<void> call(TaskEntity task) async {
+  Future<void> call(TaskEntity task) async {
     await repository.saveTask(task);
   }
-
 }
-
