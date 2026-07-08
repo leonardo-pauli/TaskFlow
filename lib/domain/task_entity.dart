0dd2438aceec010015285e50104d648ddcf4c9e6
Leonardo Pauli <96459887+leonardo-pauli@users.noreply.github.com>
2026-07-08 12:07:10 -0300

    Revert "Feat/move task"

--- lib/domain/task_entity.dart
+++ lib/domain/task_entity.dart
@@ -1,11 +1,11 @@
 enum TaskStatus { todo, doing, done }
 
-enum TaskPriority { urgent, important, medium }
+enum TaskPriority { urgent, important, normal }
 
 class TaskEntity {
   final String id;
   final String title;
-  final String description;
+  final String? description;
   final DateTime createdAt;
   final TaskStatus status;
   final TaskPriority priority;
@@ -16,7 +16,7 @@ class TaskEntity {
     required this.priority,
     required this.status,
     required this.title,
-    required this.description,
+    this.description,
   });
 
   TaskEntity copyWith({
