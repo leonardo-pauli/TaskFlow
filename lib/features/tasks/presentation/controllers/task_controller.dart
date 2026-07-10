17766508dfa0e426f9678d2dadc25d2290ac6fc8
leonardo-pauli <leonardopaulisantos@gmail.com>
2026-07-10 13:18:00 -0300

    feat(ui): implementa barra de busca reativa com filtragem em tempo real

--- lib/features/tasks/presentation/controllers/task_controller.dart
+++ lib/features/tasks/presentation/controllers/task_controller.dart
@@ -10,9 +10,17 @@ class TaskController extends ChangeNotifier {
   final UpdateTaskUseCase updateTaskuseCase;
   final DeleteTaskUseCase deleteTaskUseCase;
   final GetAllTasksUseCase getAllTasksUseCase;
+  String _searchQuery = '';
   bool isLoading = false;
   List<TaskEntity> _tasks = [];
-  List<TaskEntity> get tasks => _tasks;
+  List<TaskEntity> get tasks {
+    if(_searchQuery.isEmpty){
+      return _tasks;
+    }
+    return _tasks.where((task){
+      return task.title.toLowerCase().contains(_searchQuery.toLowerCase());
+    }).toList();
+  }
 
   TaskController({
     required this.saveTaskUseCase,
@@ -20,6 +28,11 @@ class TaskController extends ChangeNotifier {
     required this.deleteTaskUseCase,
     required this.getAllTasksUseCase,
   });
+
+  void setSearchQuery(String query){
+    _searchQuery = query;
+    notifyListeners();
+  }
 
   Future<void> loadTasks() async {
     isLoading = true;
